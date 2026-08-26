7b3ea1072ce00b40ecf4cef47a733e40d505940d
alexborodach30 <sagopien04@gmail.com>
2026-08-26 08:06:04 +0300

    ReGLU and GeGLU.

---
+++ models/activation_functions/geglu.py
@@ -0,0 +1,41 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class GEGLU(nn.Module):
+
+    def __init__(
+            self,
+            d_model: int,
+            hidden_dim: int,
+    ):
+        super().__init__()
+
+        self.w1 = nn.Linear(
+            d_model,
+            hidden_dim,
+            bias=False,
+        )
+
+        self.w2 = nn.Linear(
+            hidden_dim,
+            d_model,
+            bias=False,
+        )
+
+        self.w3 = nn.Linear(
+            d_model,
+            hidden_dim,
+            bias=False,
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        gate = self.w1(x)
+        up = self.w3(x)
+
+        hidden = (
+                F.gelu(gate)
+                * up
+        )
+
+        return self.w2(hidden)
